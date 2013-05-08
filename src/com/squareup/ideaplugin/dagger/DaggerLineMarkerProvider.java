8b245edee5fb5c5591f0b2e54d6b80d45c64db34
Jake Wharton <jw@squareup.com>
2013-05-08 00:20:22 -0700

    Almost at generic resolution.

--- src/com/squareup/ideaplugin/dagger/DaggerLineMarkerProvider.java
+++ src/com/squareup/ideaplugin/dagger/DaggerLineMarkerProvider.java
@@ -3,13 +3,19 @@ package com.squareup.ideaplugin.dagger;
 import com.intellij.codeInsight.daemon.GutterIconNavigationHandler;
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
 import com.intellij.codeInsight.daemon.LineMarkerProvider;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiClassType;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiIdentifier;
 import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiParameter;
+import com.intellij.psi.PsiType;
 import com.intellij.psi.PsiTypeElement;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiUtilBase;
 import com.intellij.ui.awt.RelativePoint;
 import com.intellij.usages.Usage;
@@ -51,7 +57,15 @@ public class DaggerLineMarkerProvider implements LineMarkerProvider {
   private static final GutterIconNavigationHandler<PsiElement> NAV_HANDLER_CTOR_INJECT_LIST =
       new GutterIconNavigationHandler<PsiElement>() {
         @Override public void navigate(MouseEvent mouseEvent, PsiElement psiElement) {
-          // TODO list types in the constructor
+          if (!(psiElement instanceof PsiMethod)) {
+            throw new IllegalStateException("Called with non-method " + psiElement);
+          }
+          PsiMethod psiMethod = (PsiMethod) psiElement;
+
+          // Get a list of the types in the constructor.
+          PsiParameter[] parameters = psiMethod.getParameterList().getParameters();
+
+          // TODO show dropdown of parameters
           // TODO   when type is clicked, use inject_to_provides
         }
       };
@@ -60,11 +74,13 @@ public class DaggerLineMarkerProvider implements LineMarkerProvider {
   private static final GutterIconNavigationHandler<PsiElement> NAV_HANDLER_PROVIDES_TO_INJECT =
       new GutterIconNavigationHandler<PsiElement>() {
         @Override public void navigate(MouseEvent mouseEvent, PsiElement psiElement) {
-          if (psiElement instanceof PsiMethod) {
-            PsiClass psiClass = PsiConsultantImpl.getReturnClassFromMethod((PsiMethod) psiElement);
-            new ShowUsagesAction(INJECT_FIELDS).startFindUsages(psiClass,
-                new RelativePoint(mouseEvent), PsiUtilBase.findEditor(psiClass), MAX_USAGES);
+          if (!(psiElement instanceof PsiMethod)) {
+            throw new IllegalStateException("Called with non-method: " + psiElement);
           }
+
+          PsiClass psiClass = PsiConsultantImpl.getReturnClassFromMethod((PsiMethod) psiElement);
+          new ShowUsagesAction(INJECT_FIELDS).startFindUsages(psiClass,
+              new RelativePoint(mouseEvent), PsiUtilBase.findEditor(psiClass), MAX_USAGES);
         }
       };
 
@@ -72,13 +88,39 @@ public class DaggerLineMarkerProvider implements LineMarkerProvider {
   private static final GutterIconNavigationHandler<PsiElement> NAV_HANDLER_INJECT_TO_PROVIDES =
       new GutterIconNavigationHandler<PsiElement>() {
         @Override public void navigate(MouseEvent mouseEvent, PsiElement psiElement) {
-          if (psiElement instanceof PsiField) {
-            PsiField psiField = (PsiField) psiElement;
-            PsiClass psiClass = PsiConsultantImpl.getClassFromField(psiField);
-            // TODO if Lazy<Foo> or Provider<Foo> extract Foo as the interesting type
-            new ShowUsagesAction(PROVIDERS).startFindUsages(psiClass, new RelativePoint(mouseEvent),
-                PsiUtilBase.findEditor(psiClass), MAX_USAGES);
+          if (!(psiElement instanceof PsiField)) {
+            throw new IllegalStateException("Called with non-field element: " + psiElement);
           }
+          PsiField psiField = (PsiField) psiElement;
+          PsiType psiFieldType = psiField.getType();
+          PsiClass psiClass = PsiConsultantImpl.getClassFromField(psiField);
+
+          if (psiFieldType instanceof PsiClassType) {
+            PsiClassType psiClassType = (PsiClassType) psiFieldType;
+
+            Project project = psiElement.getProject();
+            JavaPsiFacade javaPsiFacade = JavaPsiFacade.getInstance(project);
+            GlobalSearchScope globalSearchScope = GlobalSearchScope.allScope(project);
+
+            PsiClass lazyClass = javaPsiFacade.findClass(CLASS_LAZY, globalSearchScope);
+            PsiClass providerClass = javaPsiFacade.findClass(CLASS_PROVIDER, globalSearchScope);
+
+            PsiClassType.ClassResolveResult classResolveResult = psiClassType.resolveGenerics();
+            PsiClass outerClass = classResolveResult.getElement();
+
+            // If Lazy<Foo> or Provider<Foo>, extract Foo as the interesting type.
+            if (outerClass.equals(lazyClass) || outerClass.equals(providerClass)) {
+              PsiType genericType = classResolveResult.getSubstitutor()
+                  .getSubstitutionMap()
+                  .values()
+                  .iterator()
+                  .next();
+              // TODO convert genericType to its PsiClass and store in psiClass
+            }
+          }
+
+          new ShowUsagesAction(PROVIDERS).startFindUsages(psiClass, new RelativePoint(mouseEvent),
+              PsiUtilBase.findEditor(psiClass), MAX_USAGES);
         }
       };
 
@@ -108,8 +150,8 @@ public class DaggerLineMarkerProvider implements LineMarkerProvider {
 
       // Field injection.
       if (PsiConsultantImpl.hasAnnotation(element, CLASS_INJECT) && typeElement != null) {
-        return new LineMarkerInfo<PsiElement>(element, typeElement.getTextRange(), ICON,
-            UPDATE_ALL, null, NAV_HANDLER_INJECT_TO_PROVIDES, LEFT);
+        return new LineMarkerInfo<PsiElement>(element, typeElement.getTextRange(), ICON, UPDATE_ALL,
+            null, NAV_HANDLER_INJECT_TO_PROVIDES, LEFT);
       }
     }
 
